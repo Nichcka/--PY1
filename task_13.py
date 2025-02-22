0b35f0b503635891e31ba7bc4348f198280d8610
Nichcka <115113064+Nichcka@users.noreply.github.com>
2025-02-22 20:26:13 +0300

    Delete task_13.py

--- task_13.py
+++
@@ -1,7 +0,0 @@
-VarName = "test"  # TODO переименовать название переменной в соответствии с PEP8
-
-CONST_VAR = "old_value"
-print(CONST_VAR)
-
-const_var = "new_value"  # TODO убрать перезапись константы
-print(const_var)
